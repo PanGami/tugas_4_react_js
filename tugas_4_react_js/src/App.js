4401aefb92fc188babcebf53d5d078ff00e0be5d
Panca Nugraha Wicaksana <pancanugrahawicaksana@gmail.com>
2021-10-18 18:22:41 +0700

    Some changes

--- tugas_4_react_js/src/App.js
+++ tugas_4_react_js/src/App.js
@@ -1,25 +1,15 @@
 import React, {Component} from "react";
-import Header from "./Page/Header";
 import MenuMakanan from "./Page/MenuMakanan";
-import MenuTentangKami from "./Page/MenuTentangKami";
-import MenuKontak from "./Page/MenuKontak";
-import Footer from"./Page/Footer";
 
 class App extends Component{
   render(){
     return(
-      <div>
-        <Header/>        
+      <div>   
         <MenuMakanan/>
-        <center>
-        <MenuTentangKami tentang="Warung nusantara Adalah Restoran Yang Bernuansa Makanan Nusantara,
-        Kami Ada Untuk Anda Membuat Anda Menikmati Makanan Khas Nusantara"/>        
+        <center>     
         <p><h4>Jl. Swadaya IV, Pd.Ranggon, Cipayung, Kota Jakarta Timur, 
         Daerah Khusus Ibukota Jakarta 13860"</h4></p>
         </center>
-
-        <MenuKontak kontak="081312345678"/>
-        <Footer/>
       </div>
     );
   }
